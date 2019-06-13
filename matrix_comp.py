7c5ad7495f28db1eaadb0730842e3aa327c84a0a
Allison Mann <ainezm@bu.edu>
2019-06-13 16:12:44 -0400

    Modified matrix construction + added compute bound

--- matrix_comp.py
+++ matrix_comp.py
@@ -1,16 +1,17 @@
 import numpy as np
+import math
 
 # size is number of coin tossed (in matrix it would be n*m or number of cells)
 # n = 1 for bernoulli random var
 # p is the probability. p = 0.5 is a fair coin tossed.
 # np.random.binomial(size=10, n=1, p= 0.5)
 
-def observe_matrix(A, p):
+def observe_matrix(A, p, sigma):
 #     get size of A and initialize observation B
     num_rows = len(A); num_cols = len(A[0])
     B = np.zeros((num_rows, num_cols))
-#     generate noise of mean 0,standard deviation 1, n*m values
-    noise = np.random.normal(0,0.01,num_rows*num_cols)
+#     generate noise of mean 0,standard deviation .1, n*m values
+    noise = np.random.normal(0,sigma,num_rows*num_cols)
 #     create Chi variable (n=1, p=p, size = number of entries in the matrix)
     chi = np.random.binomial(1, p, size=num_rows*num_cols)
 #     create observation matrix
@@ -50,13 +51,34 @@ def complete_matrix(A, B,p):
 
     return (A, A_tilde)
 
+def compute_bound(n,r,p,N,A,sigma):
+    A_max = np.amax(abs(A))
+    alpha = float(n)/N
+    return math.sqrt(float(n)*r/p)*((alpha**(.75))*A_max+math.sqrt(alpha)*sigma)
 
-A = np.random.rand(50,50)
-p = 0.9
-B = observe_matrix(A, p)
+if __name__ == "__main__":
+    n = 50
+    N = 100
+    r = 5
+    sigma = .1
 
-(_, A_tilde) = complete_matrix(A, B, p)
-print(A-A_tilde)
-print(np.linalg.norm(A-A_tilde))
-print(np.linalg.norm(A-B))
+    U = np.random.rand(N,N)
+    V = np.random.rand(n,n)
+    P = np.zeros((N,n))
+    P[range(r),range(r)]=1
+    A = np.dot(U, np.dot(P, V))
+
+    p = 0.9
+    B = observe_matrix(A, p,sigma)
+
+    (_, A_tilde) = complete_matrix(A, B, p)
+    print(A-A_tilde)
+    print(np.linalg.norm(A_tilde,1))
+
+    print("bound", compute_bound(n,r,p,N,A,sigma))
+
+    print("norm of diff",np.linalg.norm((A-A_tilde),2))
+    print("min", np.amin(abs(A-A_tilde)))
+    print(np.linalg.norm(B,1))
+    print(np.linalg.norm(A,1))
 
